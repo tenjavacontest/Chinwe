9783e133ba47103158dcc78f90aee1f3045e4a79
Dom <dwi3774@googlemail.com>
2013-12-06 11:04:35 +0000

    Second git test

---
+++ src/net/chunk64/chinwe/Main.java
@@ -0,0 +1,17 @@
+package net.chunk64.chinwe;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin
+{
+
+	@Override
+	public void onEnable()
+	{
+		// perhaps something will happen here
+	}
+
+
+
+
+}
