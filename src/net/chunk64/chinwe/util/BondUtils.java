118db5e34d276fb2c9e1edc306f3f9981849958d
Dom <dwi3774@googlemail.com>
2013-12-07 10:37:53 +0000

    Finished something, must commit

--- src/net/chunk64/chinwe/util/BondUtils.java
+++ src/net/chunk64/chinwe/util/BondUtils.java
@@ -3,7 +3,9 @@ package net.chunk64.chinwe.util;
 import org.bukkit.ChatColor;
 import org.bukkit.Color;
 import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
@@ -24,11 +26,11 @@ public class BondUtils
 	}
 
 	/**
-	 * @param itemStack          The itemstack to edit
+	 * @param itemStack   The ItemStack to edit
 	 * @param name        The optional name
 	 * @param description The optional lore
 	 * @param colour      The optional leather armour colour
-	 * @return
+	 * @return The edited ItemStack
 	 */
 	public static ItemStack setInfo(ItemStack itemStack, String name, List<String> description, Color colour)
 	{
@@ -51,7 +53,7 @@ public class BondUtils
 			{
 				LeatherArmorMeta leatherArmorMeta = (LeatherArmorMeta) meta;
 				leatherArmorMeta.setColor(colour);
-//				itemStack.setItemMeta(leatherArmorMeta);
+				//				itemStack.setItemMeta(leatherArmorMeta);
 			}
 		}
 
@@ -62,6 +64,50 @@ public class BondUtils
 	public static ItemStack setInfo(Material material, String name, List<String> description, Color colour)
 	{
 		return setInfo(new ItemStack(material), name, description, colour);
+	}
+
+	/**
+	 * Gets BlockFace corresponding to the direction you're looking in
+	 */
+	public static BlockFace getDirection(LivingEntity livingEntity)
+	{
+		float yaw = livingEntity.getLocation().getYaw();
+		int angle = 45 * (Math.round(yaw / 45));
+		BlockFace blockFace;
+
+		switch (angle)
+		{
+			case 0:
+			case 360:
+				blockFace = BlockFace.SOUTH;
+				break;
+			case 45:
+				blockFace = BlockFace.SOUTH_WEST;
+				break;
+			case 90:
+				blockFace = BlockFace.WEST;
+				break;
+			case 135:
+				blockFace = BlockFace.NORTH_WEST;
+				break;
+			case 180:
+				blockFace = BlockFace.NORTH;
+				break;
+			case 225:
+				blockFace = BlockFace.NORTH_EAST;
+				break;
+			case 270:
+				blockFace = BlockFace.EAST;
+				break;
+			case 315:
+				blockFace = BlockFace.SOUTH_EAST;
+				break;
+			default:
+				blockFace = BlockFace.UP;
+		}
+
+		return blockFace.getOppositeFace();
+
 	}
 
 }
