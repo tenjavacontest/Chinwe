3c2e5f1debabaaef7ba680ff31113be0977f0fc0
Dom <dwi3774@googlemail.com>
2013-12-07 17:09:42 +0000

    Finished gadgets

--- src/net/chunk64/chinwe/gadgets/Wristwatch.java
+++ src/net/chunk64/chinwe/gadgets/Wristwatch.java
@@ -1,11 +1,18 @@
 package net.chunk64.chinwe.gadgets;
 
 import net.chunk64.chinwe.Agent;
+import net.chunk64.chinwe.util.BondUtils;
+import org.bukkit.Effect;
+import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
-import org.bukkit.util.BlockIterator;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Wristwatch extends Gadget
 {
@@ -61,8 +68,9 @@ public class Wristwatch extends Gadget
 		// start
 		laserTask = new BukkitRunnable()
 		{
-			Block targeted = null;
+			Block block = null;
 			Player player = agent.getPlayer();
+			Map<Block, Integer> damage = new HashMap<>();
 
 			@Override
 			public void run()
@@ -74,18 +82,47 @@ public class Wristwatch extends Gadget
 					return;
 				}
 
+//				BlockIterator iterator = new BlockIterator(player.getWorld(), player.getLocation().toVector(), player.getLocation().getDirection(), 0, 1);
+//				while (iterator.hasNext())
+//					block = iterator.next();
+
 				// get target
-				BlockIterator iterator = new BlockIterator(player.getWorld(), player.getLocation().toVector(), player.getLocation().getDirection(), 0, 4);
+				block = player.getTargetBlock(null, 2); // easier
 
-				while (iterator.hasNext())
-					targeted = iterator.next();
 
-				// TODO laser
+				// check you're looking at a block
+				if (block != null && block.getType() != Material.AIR)
+				{
+					if (damage.containsKey(block))
+					{
+						int damaged = damage.get(block);
 
+						// should it break?
+						if (damaged < 2)
+						{
+							// yus
+							damage.remove(block);
+							block.breakNaturally();
+						} else
+						{
+							// just decrement damage
+							damage.put(block, damage.get(block) - 2);
+						}
+					}
+
+					else
+					{
+						damage.put(block, block.getType() == Material.OBSIDIAN || block.getType() == Material.BEDROCK ? 8 : 4);
+						block.getWorld().playEffect(block.getLocation(), Effect.STEP_SOUND, Material.REDSTONE_BLOCK);
+						block.getWorld().playEffect(player.getLocation(), Effect.SMOKE, BondUtils.getDirection(player.getLocation()));
+						block.getWorld().playEffect(block.getLocation(), Effect.SMOKE, BlockFace.UP);
+						block.getWorld().playSound(block.getLocation(), Sound.FUSE, 0.5F, 0.1F);
+					}
+				}
 
 
 			}
-		}.runTaskTimer(plugin, 0L, 2L);
+		}.runTaskTimer(plugin, 0L, 5L);
 
 
 	}
